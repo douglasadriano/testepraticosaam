947108c571e16eb82f5b1985f71083e585aef564
Douglas Adriano <da-zf@hotmail.com>
2025-05-04 17:27:17 -0300

    Primeiro Commit.

---
+++ src/testepraticosaam/TestePraticoSAAM.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testepraticosaam;
+
+import javax.swing.SwingUtilities;
+import testepraticosaam.view.TelaLoginCadastro;
+
+public class TestePraticoSAAM {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TelaLoginCadastro login = new TelaLoginCadastro();
+            login.setVisible(true);
+        });
+    }
+}
